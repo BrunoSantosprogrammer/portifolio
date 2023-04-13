9b79b2c1b50ea4a950b273c139a7668b80ce87a6
Bruno Santos <rapazcomumvl@hotmail.com>
2023-04-13 10:23:18 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/brunosantosprogrammer.png" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Title from '../../components/Title'
 
+import { Asidebar, Descricao, BotaoTema, SidebarContainer } from './styles'
+
 const Sidebar = () => (
-  <aside>
-    <img src="https://github.com/brunosantosprogrammer.png" />
-    <Title fontSize={20}>Bruno Santos</Title>
-  </aside>
+  <Asidebar>
+    <SidebarContainer>
+      <Avatar />
+      <Title fontSize={19}>Bruno Santos</Title>
+      <Paragrafo tipo="secundario" fontSize={14}>
+        brunosantosprogrammer
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Estudante Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
+  </Asidebar>
 )
 
 export default Sidebar
